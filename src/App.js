396f2359a608f5177e2ff901a989d7df48492e39
codewithrich <richardson12230@gmail.com>
2024-03-28 23:59:54 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
+import Axios from "axios";
 
-function App() {
+const KEY='Enter your API key';
+
+const App = () => {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`
+      );
+      setData(response.data);
+    } 
+    catch (err) {
+      console.log(err);
+      alert(err.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Weather Web App</h1>
+        <div className="inpCity">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)} //changes the already present value in usestate
+          placeholder="Enter city name"
+        />
+        <button onClick={fetchData}>Search</button>
+        </div>
+        <div> 
+          {data && <div className="container">
+          
+            <ul> Place: {data.name}</ul>
+            <ul> Temperature: {Math.round(data.main.temp)}</ul>
+            <ul> latitude: {data.coord.lat}</ul>
+            <ul> Longitute: {data.coord.lon}</ul>
+            
+          
+          </div>}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
